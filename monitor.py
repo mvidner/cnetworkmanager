f05ea1e97bd4dd4e7b0cc709c7d247caee149b0a
Martin Vidner <martin@vidner.net>
2009-07-10 23:28:32 +0200

    Split off the monitor.

--- monitor.py
+++ monitor.py
@@ -1,57 +1,62 @@
-import string
+from monitor_base import MonitorBase
+from manager import cNM
+from device07 import cDevice_07
 
-class MonitorBase:
+class Monitor(MonitorBase):
     def __init__(self, bus):
-        self.bus = bus
+        MonitorBase.__init__(self, bus)
 
-        self.amap = {}
-        self.specific = {}
-        self.bus.add_signal_receiver(self.abbr_h,
-                        path_keyword="path",
-                        interface_keyword="interface",
-                        member_keyword="member")
+        self.watch(
+            self.propc_h,
+            dbus_interface="org.freedesktop.NetworkManager.Device.Wireless",
+            signal_name="PropertiesChanged")
+        self.watch(
+            self.propc_h,
+            dbus_interface="org.freedesktop.NetworkManager.AccessPoint",
+            signal_name="PropertiesChanged")
 
-    def ignore(self, dbus_interface, signal_name):
-        self.watch(self.null_h, dbus_interface, signal_name)
+        self.ignore("org.freedesktop.Hal.Device", "PropertyModified")
+        self.ignore("fi.epitest.hostap.WPASupplicant.Interface", "ScanResultsAvailable")
+        self.ignore("com.redhat.PrinterSpooler", "QueueChanged")
+        self.ignore("org.freedesktop.NetworkManager", "StateChange") # deprecated
+        self.watch(self.nm_sc_h, "org.freedesktop.NetworkManager", "StateChanged")
+        self.watch(self.wpas_isc_h, "fi.epitest.hostap.WPASupplicant.Interface", "StateChange")
+        self.watch(self.nmd_sc_h, "org.freedesktop.NetworkManager.Device", "StateChanged")
+        self.watch(self.bus_noc_h, "org.freedesktop.DBus", "NameOwnerChanged")
 
-    def null_h(self, *args, **kwargs):
-        pass
-
-    def watch(self, handler, dbus_interface, signal_name):
-        self.specific[dbus_interface +"."+ signal_name] = True
-        self.bus.add_signal_receiver(handler,
-                                dbus_interface=dbus_interface,
-                                signal_name=signal_name,
-                                path_keyword="path")
-
-    def abbr_h(self, *args, **kwargs):
-        ifc = kwargs["interface"]
-        sig = kwargs["member"]
-        if self.specific.has_key(ifc +"."+ sig):
-            return
-
-        opath = kwargs["path"]
-        line = "SIG %s: %s.%s%s" % (self.abbrev(opath,"/"),
-                                    self.abbrev(ifc,"."),
-                                    sig, args)
-        print line
-
-    def abbrev(self, s, sep):
-        words = s.split(sep)
-        words = map (self.a1, words)
-        result = sep.join(words)
-        if self.amap.has_key(result):
-            if self.amap[result] != s:
-                print "ABBR COLLISION %s was %s now %s" % (result, self.amap[result], s)
+    def bus_noc_h(self, *args, **kwargs):
+        (name, old, new) = args
+        if new == "":
+            new = "gone"
         else:
-            print "ABBR %s is %s" % (result, s)
-        self.amap[result] = s
-        return result
-    
-    def a1(self, s):
-        if s == "":
-            return ""
-        #print "#A", s
-        # first char, delete lowercase and _ from the rest
-        return s[0] + s[1:].translate(string.maketrans("", ""),
-                                      string.lowercase + "_")
+            new = "at " + new
+        print "\tBUS NOC\t%s %s" % (name, new)
+
+    def wpas_isc_h(self, *args, **kwargs):
+        opath = kwargs["path"]
+        (new, old) = args
+        print "\tWPAS %s\t(%s, was %s)" % (new, opath, old.lower())
+
+    def nmd_sc_h(self, *args, **kwargs):
+        opath = kwargs["path"]
+        (new, old, reason) = args
+        news = cDevice_07.NM_DEVICE_STATE[new]
+        olds = cDevice_07.NM_DEVICE_STATE[old]
+        reasons = ""
+        if reason != 0:
+            reasons = "reason %d" % reason
+        print "\tDevice State %s\t(%s, was %s%s)" % (news, opath, olds.lower(), reasons)
+
+    def nm_sc_h(self, *args, **kwargs):
+        s = args[0]
+        ss = cNM.NM_STATE[s]
+        print "\tNM State:", ss
+
+    def propc_h(self, *args, **kwargs):
+        opath = kwargs["path"]
+        props = args[0]
+        for k, v in props.iteritems():
+            if k == "Strength":
+                v = "%u" % v
+            line = "\tPROP\t%s\t%s\t(%s)" % (k, v, opath)
+            print line
