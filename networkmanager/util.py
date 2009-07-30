4445c40f091c49c8f6ffadd68c289f211b9c8ddd
Martin Vidner <martin@vidner.net>
2009-07-30 12:08:39 +0200

    Cleaned up networkmanager.util.
    Added docstrings with doctest.
    Enum returns str(value) instead of "?" for unknown values.
    Flags translates 0 to its name.

--- networkmanager/util.py
+++ networkmanager/util.py
@@ -1,34 +1,73 @@
 class NamedNumbers:
+    """Base for Enum and Flags."""
+
     def __init__(self, value):
         self.value = value
     def __int__(self):
+        """
+        >>> n = NamedNumbers(42)
+
+        >>> int(n)
+        42
+        """
         return self.value
 
 class Enum(NamedNumbers):
-    """enum
+    """Enumeration."""
 
-    class Foo(Enum):
-        FOO=1
-        BAR=2
-
-    >>> e = Foo(Foo.BAR)
-    >>> str(e)
-    "BAR"
-    """
     def __str__(self):
+        """
+        >>> class Foo(Enum):
+        ...    ONE = 1
+        ...    TWO = 2
+
+        >>> Foo.ONE
+        1
+
+        >>> str(Foo(Foo.TWO))
+        'TWO'
+
+        >>> str(Foo(3))
+        '3'
+        """
+
         for n, v in self.__class__.__dict__.iteritems():
             if v == self.value:
                 return n
-        return "?"
+        return str(self.value)
     # TODO __repr__
 
 class Flags(NamedNumbers):
+    """Bit flags."""
+
     def __str__(self):
+        """
+        >>> class MyFlags(Flags):
+        ...     NONE = 0x0
+        ...     EXECUTE = 0x1
+        ...     WRITE = 0x2
+        ...     READ = 0x4
+    
+        >>> str(MyFlags(5))
+        'EXECUTE,READ'
+    
+        >>> str(MyFlags(0))
+        'NONE'
+    
+        >>> str(MyFlags(9)) # doctest: +SKIP
+        'EXECUTE,0x8'    
+        """
+
         has = {}
         for n, v in self.__class__.__dict__.iteritems():
-            if isinstance(v, int) and self.value & v: # FIXME long
-                has[v] = n
+            if isinstance(v, int): # FIXME long
+                if self.value & v or (self.value == 0 and v == 0):
+                    has[v] = n
+                    
         names = [has[v] for v in sorted(has.keys())]
-        # TODO zero is a special case
         # TODO unknown values?
         return ",".join(names)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
